68b50ec12863c2fb0fb8de52efe0728f9287ba93
Alexander Afanasiev <armawir1@mail.ru>
2023-09-14 16:16:19 +0700

    Updates

---
+++ precache-manifest.2fb0b2082ff8f027442f7bc1236189f0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "697b107ce4dfb1bbfac3",
+    "url": "/react-burger.github.io/static/css/main.8b85977a.chunk.css"
+  },
+  {
+    "revision": "697b107ce4dfb1bbfac3",
+    "url": "/react-burger.github.io/static/js/main.697b107c.chunk.js"
+  },
+  {
+    "revision": "dbc81d6accd7b44ce102",
+    "url": "/react-burger.github.io/static/js/1.dbc81d6a.chunk.js"
+  },
+  {
+    "revision": "3fe187ae2894e3a2d79a",
+    "url": "/react-burger.github.io/static/css/2.deb49dcb.chunk.css"
+  },
+  {
+    "revision": "3fe187ae2894e3a2d79a",
+    "url": "/react-burger.github.io/static/js/2.3fe187ae.chunk.js"
+  },
+  {
+    "revision": "88931c4da376ba1ad7f1",
+    "url": "/react-burger.github.io/static/js/runtime~main.88931c4d.js"
+  },
+  {
+    "revision": "d956cbee59c6683e3bcfe8e65a22562e",
+    "url": "/react-burger.github.io/static/media/logo.d956cbee.svg"
+  },
+  {
+    "revision": "0377dc821826f785b692e46614b8da0f",
+    "url": "/react-burger.github.io/static/media/Subtract.0377dc82.svg"
+  },
+  {
+    "revision": "889f30de490b10c974e128d02ec92dda",
+    "url": "/react-burger.github.io/static/media/done1.889f30de.svg"
+  },
+  {
+    "revision": "21ba2c11275a07ccf3890fbeab585525",
+    "url": "/react-burger.github.io/static/media/JetBrainsMono-Regular.21ba2c11.woff2"
+  },
+  {
+    "revision": "2fe9a2bdaa6daf61e770a2edb7252040",
+    "url": "/react-burger.github.io/static/media/JetBrainsMono-Regular.2fe9a2bd.woff"
+  },
+  {
+    "revision": "a492f7427b1561cc96ace88f5756d94e",
+    "url": "/react-burger.github.io/static/media/JetBrainsMono-Bold.a492f742.woff2"
+  },
+  {
+    "revision": "44e2d48cffc1c5da01730f6103fe9eaa",
+    "url": "/react-burger.github.io/static/media/iceland.44e2d48c.woff2"
+  },
+  {
+    "revision": "896f23850b33cd1df94eb955d4b60079",
+    "url": "/react-burger.github.io/static/media/JetBrainsMono-Bold.896f2385.woff"
+  },
+  {
+    "revision": "df042be2e40babc665f1f8a2d67d0f86",
+    "url": "/react-burger.github.io/static/media/iceland.df042be2.woff"
+  },
+  {
+    "revision": "143037b3a2aca1874a437732d3db13c7",
+    "url": "/react-burger.github.io/index.html"
+  }
+];
