284fc9dc6f39e99c55012531fdb3dfbcf9be8cfd
Alexander Afanasiev <armawir1@mail.ru>
2023-09-14 16:23:48 +0700

    Updates

---
+++ precache-manifest.e800cc8f3496d566701eeae42f473a62.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "697b107ce4dfb1bbfac3",
+    "url": "/react-burger/static/css/main.8b85977a.chunk.css"
+  },
+  {
+    "revision": "697b107ce4dfb1bbfac3",
+    "url": "/react-burger/static/js/main.697b107c.chunk.js"
+  },
+  {
+    "revision": "dbc81d6accd7b44ce102",
+    "url": "/react-burger/static/js/1.dbc81d6a.chunk.js"
+  },
+  {
+    "revision": "4fb50a06c0be490bde47",
+    "url": "/react-burger/static/css/2.0b1849e4.chunk.css"
+  },
+  {
+    "revision": "4fb50a06c0be490bde47",
+    "url": "/react-burger/static/js/2.4fb50a06.chunk.js"
+  },
+  {
+    "revision": "1ada705cd771288ccc10",
+    "url": "/react-burger/static/js/runtime~main.1ada705c.js"
+  },
+  {
+    "revision": "d956cbee59c6683e3bcfe8e65a22562e",
+    "url": "/react-burger/static/media/logo.d956cbee.svg"
+  },
+  {
+    "revision": "0377dc821826f785b692e46614b8da0f",
+    "url": "/react-burger/static/media/Subtract.0377dc82.svg"
+  },
+  {
+    "revision": "889f30de490b10c974e128d02ec92dda",
+    "url": "/react-burger/static/media/done1.889f30de.svg"
+  },
+  {
+    "revision": "21ba2c11275a07ccf3890fbeab585525",
+    "url": "/react-burger/static/media/JetBrainsMono-Regular.21ba2c11.woff2"
+  },
+  {
+    "revision": "a492f7427b1561cc96ace88f5756d94e",
+    "url": "/react-burger/static/media/JetBrainsMono-Bold.a492f742.woff2"
+  },
+  {
+    "revision": "896f23850b33cd1df94eb955d4b60079",
+    "url": "/react-burger/static/media/JetBrainsMono-Bold.896f2385.woff"
+  },
+  {
+    "revision": "2fe9a2bdaa6daf61e770a2edb7252040",
+    "url": "/react-burger/static/media/JetBrainsMono-Regular.2fe9a2bd.woff"
+  },
+  {
+    "revision": "44e2d48cffc1c5da01730f6103fe9eaa",
+    "url": "/react-burger/static/media/iceland.44e2d48c.woff2"
+  },
+  {
+    "revision": "df042be2e40babc665f1f8a2d67d0f86",
+    "url": "/react-burger/static/media/iceland.df042be2.woff"
+  },
+  {
+    "revision": "9d902f3fba2624cdcf75855b41d64730",
+    "url": "/react-burger/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-burger.github.io/precache-manifest.2fb0b2082ff8f027442f7bc1236189f0.js"
+  "/react-burger/precache-manifest.e800cc8f3496d566701eeae42f473a62.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-burger.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/react-burger/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
